9b18294c18c3fe86ad1873a3d33182dee36203f8
GabrielCoghi14 <coghigabriel14@gmail.com>
2024-04-08 19:13:19 -0300

    Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,14 @@ document.querySelector("#prototipo_nao").addEventListener("change", calcular)
 
 document.querySelector("#prazo").addEventListener("input", function(){
     const prazo = document.querySelector("#prazo").value
-    document.querySelector("label[for=prazo]").innerText = "Prazo:" + prazo
+    document.querySelector("label[for=prazo]").innerText = "Risco:" + prazo
     calcular()
 })
 
 function calcular(){
     
     const qtdeDePaginas = document.querySelector("#paginas").value
-    let valor = qtdeDePaginas*1000
+    let valor = qtdeDePaginas*10
 
     const tipo = document.querySelector("tipo").value
     if(tipo == 2) valor += 2000
